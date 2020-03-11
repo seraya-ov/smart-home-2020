ddf3aaf0c052b7232481c08ced54c531ca03e087
Olga Seraya <seraya.ov@phystech.edu>
2020-03-11 17:13:23 +0300

    fix_reader

--- src/main/java/ru/sbt/mipt/oop/SmartHomeReader.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeReader.java
@@ -7,9 +7,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SmartHomeReader {
-    public SmartHome getSmartHomeFromJson(String path) throws IOException {
-        Gson gson = new Gson();
-        String json = new String(Files.readAllBytes(Paths.get(path)));
-        return gson.fromJson(json, SmartHome.class);
+    public SmartHome getSmartHomeFromJson(String path) {
+        try {
+            Gson gson = new Gson();
+            String json = new String(Files.readAllBytes(Paths.get(path)));
+            return gson.fromJson(json, SmartHome.class);
+        }
+        catch (IOException exc) {
+            System.out.println("Unable to read Smart Home from json. " + exc.getMessage());
+            return null;
+        }
     }
 }
