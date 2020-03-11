22f3767b2c3ddcc5dc92af1ad0619a16796e50b4
Olga Seraya <seraya.ov@phystech.edu>
2020-03-11 17:16:27 +0300

    update

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,12 +1,11 @@
 package ru.sbt.mipt.oop;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class Application {
 
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) {
         SmartHome smartHome = new SmartHomeReader().getSmartHomeFromJson("output.js");
         NextSensorEventGenerator nextSensorEventGenerator = new NextSensorEventGenerator();
         List<EventHandler> eventHandlers = Arrays.asList(new DoorEventHandler(smartHome), new LightEventHandler(smartHome), new HallDoorEventHandler(smartHome));
