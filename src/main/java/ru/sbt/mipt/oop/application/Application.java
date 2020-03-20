9d1b53240f259125e2c46c3484e38c8d9564851b
Olga Seraya <seraya.ov@phystech.edu>
2020-03-20 20:08:05 +0300

    add packages

--- src/main/java/ru/sbt/mipt/oop/application/Application.java
+++ src/main/java/ru/sbt/mipt/oop/application/Application.java
@@ -5,6 +5,8 @@ import ru.sbt.mipt.oop.handlers.sensor_event_handlers.DoorEventHandler;
 import ru.sbt.mipt.oop.handlers.sensor_event_handlers.HallDoorEventHandler;
 import ru.sbt.mipt.oop.handlers.sensor_event_handlers.LightEventHandler;
 import ru.sbt.mipt.oop.handlers.sensor_event_handlers.SensorEventHandler;
+import ru.sbt.mipt.oop.handlers.sensor_event_handlers.decorators.AlarmSensorEventHandler;
+import ru.sbt.mipt.oop.objects.alarm.SmartAlarm;
 import ru.sbt.mipt.oop.objects.home_objects.actionable.SmartHome;
 import ru.sbt.mipt.oop.readers.SmartHomeReader;
 
@@ -15,7 +17,8 @@ public class Application {
     public static void main(String... args) {
         SmartHome smartHome = new SmartHomeReader().getSmartHomeFromJson("output.js");
         NextSensorEventGenerator nextSensorEventGenerator = new NextSensorEventGenerator();
-        List<SensorEventHandler> sensorEventHandlers = Arrays.asList(new DoorEventHandler(smartHome), new LightEventHandler(smartHome), new HallDoorEventHandler(smartHome));
+        SmartAlarm alarm = new SmartAlarm();
+        List<SensorEventHandler> sensorEventHandlers = Arrays.asList(new AlarmSensorEventHandler(alarm, new DoorEventHandler(smartHome)), new AlarmSensorEventHandler(alarm, new LightEventHandler(smartHome)), new AlarmSensorEventHandler(alarm, new HallDoorEventHandler(smartHome)));
         new EventSequence(nextSensorEventGenerator, sensorEventHandlers).run();
     }
 }
